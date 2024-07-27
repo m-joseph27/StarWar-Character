42c0a6f05468c0e82005c65d72d0131929e98bd5
Muhammad Yusuf <muhammadyusuf@MacBook-Air-Muhammad.local>
2024-07-27 11:07:28 +0700

    create navigation bar

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss'],
+  standalone: true,
+  imports: [CommonModule]
+})
+export class NavbarComponent {
+  menuOpen = false;
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+}
