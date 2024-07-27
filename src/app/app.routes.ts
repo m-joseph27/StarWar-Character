9ff5857b140a336ecb25cea0780b681f87cbd975
Muhammad Yusuf <muhammadyusuf@MacBook-Air-Muhammad.local>
2024-07-27 20:31:31 +0700

    create routing for the menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { CardComponent } from './components/card/card.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/people', pathMatch: 'full' },
+  { path: 'people', component: CardComponent },
+];
