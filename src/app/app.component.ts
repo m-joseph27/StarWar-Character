8bc161c2e27b6da0f85f4bf961c67edd35203656
Muhammad Yusuf <muhammadyusuf@MacBook-Air-Muhammad.local>
2024-07-27 17:14:17 +0700

    working on create base card and navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { CommonModule } from '@angular/common';
+import { CardComponent } from './components/card/card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [
+    RouterOutlet,
+    CommonModule,
+    NavbarComponent,
+    CardComponent,
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,12 +1,86 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, CardModule, ButtonModule],
   templateUrl: './card.component.html',
-  styleUrl: './card.component.scss'
+  styleUrls: ['./card.component.scss']
 })
 export class CardComponent {
-
+  characters = [
+    {
+      name: "Luke Skywalker",
+      height: "172",
+      mass: "77",
+      hair_color: "blond",
+      skin_color: "fair",
+      eye_color: "blue",
+      birth_year: "19BBY",
+      gender: "male",
+    },
+    {
+      name: "Darth Vader",
+      height: "202",
+      mass: "136",
+      hair_color: "none",
+      skin_color: "white",
+      eye_color: "yellow",
+      birth_year: "41.9BBY",
+      gender: "male",
+    },
+    {
+      name: "Leia Organa",
+      height: "150",
+      mass: "49",
+      hair_color: "brown",
+      skin_color: "light",
+      eye_color: "brown",
+      birth_year: "19BBY",
+      gender: "female",
+    },
+    {
+      name: "Leia Organa",
+      height: "150",
+      mass: "49",
+      hair_color: "brown",
+      skin_color: "light",
+      eye_color: "brown",
+      birth_year: "19BBY",
+      gender: "female",
+    },
+    {
+      name: "Leia Organa",
+      height: "150",
+      mass: "49",
+      hair_color: "brown",
+      skin_color: "light",
+      eye_color: "brown",
+      birth_year: "19BBY",
+      gender: "female",
+    },
+    {
+      name: "Leia Organa",
+      height: "150",
+      mass: "49",
+      hair_color: "brown",
+      skin_color: "light",
+      eye_color: "brown",
+      birth_year: "19BBY",
+      gender: "female",
+    },
+    {
+      name: "Luke Skywalker",
+      height: "172",
+      mass: "77",
+      hair_color: "blond",
+      skin_color: "fair",
+      eye_color: "blue",
+      birth_year: "19BBY",
+      gender: "male",
+    },
+  ];
 }
