a09a85e381c7c2a1af10ef4c60c716218dff4fc5
Muhammad Yusuf <muhammadyusuf@MacBook-Air-Muhammad.local>
2024-07-27 22:18:23 +0700

    fix style detail and navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -11,7 +12,15 @@ import { Component } from '@angular/core';
 export class NavbarComponent {
   menuOpen = false;
 
+  constructor(
+    private router: Router
+  ) {}
+
   toggleMenu() {
     this.menuOpen = !this.menuOpen;
+  }
+
+  backToHome() {
+    this.router.navigate(['/']);
   }
 }
