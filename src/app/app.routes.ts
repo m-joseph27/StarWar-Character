2ff984f89e6b440e6f48d128f917aab715906119
Muhammad Yusuf <muhammadyusuf@MacBook-Air-Muhammad.local>
2024-07-29 07:08:54 +0700

    fix sub folder film

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { CardComponent } from './components/card/card.component';
 import { PeopleDetailComponent } from './views/people-detail/people-detail.component';
-import { FilmDetailComponent } from './components/film-detail/film-detail.component';
+import { FilmDetailComponent } from './views/film-detail/film-detail.component';
 import { PlanetDetailComponent } from './views/planet-detail/planet-detail.component';
 import { VehicleDetailComponent } from './views/vehicle-detail/vehicle-detail.component';
 import { StarshipsDetailComponent } from './views/starships-detail/starships-detail.component';
