92bd5185781b59ad5e5be0eaa8ce8f88786d416d
Muhammad Yusuf <muhammadyusuf@MacBook-Air-Muhammad.local>
2024-07-29 07:04:16 +0700

    fix sub folder

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { CardComponent } from './components/card/card.component';
-import { PeopleDetailComponent } from './components/people-detail/people-detail.component';
+import { PeopleDetailComponent } from './views/people-detail/people-detail.component';
 import { FilmDetailComponent } from './components/film-detail/film-detail.component';
 import { PlanetDetailComponent } from './views/planet-detail/planet-detail.component';
 import { VehicleDetailComponent } from './views/vehicle-detail/vehicle-detail.component';
